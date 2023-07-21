d94d185bb42c8acd35a009fdac23a33ec9c66107
mufeng233 <2441260435@qq.com>
2023-07-21 21:56:39 +0800

    Update new files

---
+++ src/com/mufeng/test/CharacterTest.java
@@ -0,0 +1,8 @@
+package com.mufeng.test;
+
+public class Character {
+    public static void main(String[] args) {
+        Character ch = 'a';
+        System.out.println(ch.);
+    }
+}
---
+++ src/com/mufeng/test/MathTest.java
@@ -0,0 +1,2 @@
+package com.mufeng.test;public class MathTest {
+}
