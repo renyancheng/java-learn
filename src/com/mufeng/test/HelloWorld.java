f9e4317e69ba817208f7d3cc26aeb1184bdb279f
mufeng233 <2441260435@qq.com>
2023-10-05 10:15:41 +0800

    feat: HelloWorld

---
+++ src/com/mufeng/test/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.mufeng.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
