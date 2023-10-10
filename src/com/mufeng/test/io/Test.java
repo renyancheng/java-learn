4f477f4e5315d0610f415f06bc0d1b025a1ee1bc
mufeng233 <2441260435@qq.com>
2023-10-10 16:00:21 +0800

    feat: 添加一个用来测试的序列化对象

---
+++ src/com/mufeng/test/io/Test.java
@@ -0,0 +1,9 @@
+package com.mufeng.test.io;
+
+import java.io.Serializable;
+
+public class Test implements Serializable {
+    public void print() {
+        System.out.println("test ok!!!");
+    }
+}
