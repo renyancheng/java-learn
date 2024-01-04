f4283da8b56de4fb168278e49a55c68a9b7213c3
mufeng233 <2441260435@qq.com>
2024-01-04 20:18:39 +0800

    feat: 玩一下lombok logger

---
+++ src/com/mufeng/test/logger/LombokLogger.java
@@ -0,0 +1,10 @@
+package com.mufeng.test.logger;
+
+import lombok.extern.java.Log;
+
+@Log
+public class LombokLogger {
+    public static void main(String[] args) {
+        log.info("你好");
+    }
+}
